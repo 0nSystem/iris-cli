b5b8874e55236a32c62767d3fdc1d146b5994784
dgarcia <danielgsu@ext.inditex.com>
2023-05-04 13:37:26 +0200

    added handler error in main function

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ async fn main() -> Result<(), Report> {
     color_eyre::install()?;
     //cli::Cli::parse_from(vec!["","--help"]);
 
-    let arg_cli = cli::Cli::parse_from(vec![   "","-v","-c","./default_config_file.json","-l","ES","text","Hello World"]);
+    let arg_cli = cli::Cli::parse_from(vec![   "","-c","./default_config_file.json","-l","ES","text","Hello World"]);
 
     //Example create template
     //let arg_cli = cli::Cli::parse_from(vec!["", "-vv", "-e", "./config_file.json", "template"]);
@@ -17,10 +17,14 @@ async fn main() -> Result<(), Report> {
 
     log::info!("Starting translation-cli");
 
+    //TODO
     match task_procces::start_procces(&arg_cli).await {
-        Ok(_) => Ok(()),
-        Err(_er) => todo!(),
-    }
+        Ok(_) => {},
+        Err(task_error) => {
+            log::error!("{}",task_procces::handler_task_error(task_error));
+        }
+    };
+    Ok(())
 }
 
 /*
