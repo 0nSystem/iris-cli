2cc99421d96a95774e038618d9c43aa2ca2fde83
onsystem <thehiddenonsystem@proton.me>
2023-06-12 23:18:27 +0200

    correction replace with regex pattern and match fields regex expression in update

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,7 @@ use iris_cli::{cli, task_procces};
 #[tokio::main]
 async fn main() -> Result<(), Report> {
     color_eyre::install()?;
-    //let arg_cli = cli::Cli::parse();
-    let arg_cli = cli::Cli {
-        command: cli::Commands::Sql {
-            field_index: "0,1".to_string(),
-            mode: cli::ModeSql::Insert,
-        },
-        config: Some("./default_config_file.json".parse()?),
-        export: None,
-        verbose: 0,
-        language: Some("ES".to_owned()),
-        file: Some("./test_sql.sql".parse()?),
-    };
+    let arg_cli = cli::Cli::parse();
 
     config_logger(arg_cli.verbose, env_logger::Target::Stdout).expect("Error config logger");
 
--- test_sql.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO TABLE_NAME(Nombre,Apellidos) VALUES ('Hello','Bye');
-INSERT INTO TABLE_NAME(Nombre,Apellidos) VALUES ('Hello World','Bye boys'),
-('Father','Sister');
-INSERT INTO TABLE_NAME VALUES ('Hello World','Bye boys');
