b5426d0d5ebd708bf40e76a7c94012fb141a9bbe
onsystem <onsystem@proton.me>
2023-06-16 00:10:04 +0200

    refactor text procces

--- src/task_procces/text_procces.rs
+++ src/task_procces/text_procces.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use color_eyre::{Report, Result};
 
 use crate::petitions::client::options_request_client;
-use crate::petitions::{client, management_response};
+use crate::petitions::{client, management_response, translation};
 use crate::system_resources::{actions::get_file_to_string, model::config_file::ConfigFile};
 
 pub async fn config_and_run_text_command(
@@ -34,30 +34,18 @@ async fn text_command_procces(
     for (index, config) in config.configurations.iter().enumerate() {
         //TODO remove clone
         let name_file_to_add = config.name.clone().unwrap_or_else(|| index.to_string());
-        match client::build_client::build_client(config.authentication.as_ref()) {
-            Ok(client) => {
-                let options_request = &options_request_client::OptionClientRequest::from(config);
-                match management_response::create_and_management_response(
-                    &client,
-                    options_request,
-                    text,
-                    languaje,
-                    &config.get_value_json,
-                )
-                .await
-                {
-                    Ok(value_request) => {
-                        if let Some(value_verification) = value_request.1.first() {
-                            map_name_file_to_add_and_value_info_translate
-                                .insert(name_file_to_add, value_verification.clone());
-                            //TODO clone
-                        };
-                    }
-                    Err(_) => todo!(),
-                }
-            }
-            Err(_) => todo!(),
-        };
+        let client = client::build_client::build_client(config.authentication.as_ref())?;
+        let options_request = &options_request_client::OptionClientRequest::from(config);
+
+        let translate = translation(
+            &client,
+            options_request,
+            text,
+            languaje,
+            &config.get_value_json,
+        )
+        .await?;
+        map_name_file_to_add_and_value_info_translate.insert(name_file_to_add, translate);
     }
 
     Ok(map_name_file_to_add_and_value_info_translate)
